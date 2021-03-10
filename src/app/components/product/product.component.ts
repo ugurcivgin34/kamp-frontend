79529b7e0a12868545c01f2a6daf7a4ee2b2b618
okan çivgin <okanbjkspd@hotmail.com>
2021-03-10 23:17:26 +0300

    Ders sonu

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,19 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { HttpClient } from '@angular/common/http'; //api ye ulaşabileceğiz
+import { ProductResponseModel } from 'src/app/models/productsResponseModel';
+
+//axios,fetch react tarafında
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-
- 
-
-  products:Product[] = [];
-  constructor() { }
-
-  ngOnInit(): void {
+  products: Product[] = [];
+  apiUrl = 'https://localhost:44318/api/products/getall';
+  productResponseModel:ProductResponseModel = {
+    data : this.products,
+    message:"",
+    success:true
   }
 
+  constructor(private httpClient: HttpClient) {} // ProductComponenti bellekte oluşturmaktadır.instance ni üretmektir.yani new lemektir
+  //C# dependency injeksion servisleri yaptığımız yer burası
+
+  ngOnInit(): void {
+    //console.log('Init çalıştı');
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.httpClient
+      .get<ProductResponseModel>(this.apiUrl)
+      .subscribe((response) => {
+        this.products=response.data
+
+      });
+      
+  }
 }
